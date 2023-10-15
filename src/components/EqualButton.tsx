20e4de1326d92f3492d15cd859ef6db4daef9ed5
Omar-Code-Coding <omaryasser_250@hotmail.com>
2023-10-15 16:50:16 +0300

    add equal button component

---
+++ src/components/EqualButton.tsx
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import styles from "./EqualButton.module.css";
+function EqualButton() {
+  return <Button className={styles["btn-equal"]}>=</Button>;
+}
+
+export default EqualButton;
